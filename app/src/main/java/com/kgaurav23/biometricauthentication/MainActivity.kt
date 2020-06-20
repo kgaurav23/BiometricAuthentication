9190441bbbcc5e4e1e88ec2aa1f68a45ed6b1f5e
Kumar Gaurav <kumar.gaurav1@citrix.com>
2020-06-20 18:40:23 +0530

    - basic implementation done for biometric auth

--- app/src/main/java/com/kgaurav23/biometricauthentication/MainActivity.kt
+++ app/src/main/java/com/kgaurav23/biometricauthentication/MainActivity.kt
@@ -1,12 +1,70 @@
 package com.kgaurav23.biometricauthentication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        verifyBioMetricAuthExistence()
+
+        val executor = ContextCompat.getMainExecutor(this)
+        val biometricPrompt = BiometricPrompt(this, executor, object : BiometricPrompt.AuthenticationCallback() {
+
+            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                super.onAuthenticationError(errorCode, errString)
+                if (errorCode == BiometricPrompt.ERROR_NEGATIVE_BUTTON) {
+                    // user clicked negative/cancel button
+                    Toast.makeText(this@MainActivity,"Cancel Button clicked!", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this@MainActivity,"Unrecoverable error", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                super.onAuthenticationSucceeded(result)
+                runOnUiThread{
+                    Toast.makeText(this@MainActivity,"Authentication succeeded!", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onAuthenticationFailed() {
+                super.onAuthenticationFailed()
+                runOnUiThread{
+                    Toast.makeText(this@MainActivity,"Authentication failed! Please try again.", Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+
+        val promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Authentication prompt!")
+            /*Subtitle and description are optional parameters, so, you can skip those parameters.
+            .setSubtitle("Set the subtitle to display.")
+            .setDescription("Verification required")*/
+            .setNegativeButtonText("Cancel")
+            .build()
+
+        biometricPrompt.authenticate(promptInfo)
+    }
+
+    private fun verifyBioMetricAuthExistence() {
+        val biometricManager = BiometricManager.from(this)
+        when (biometricManager.canAuthenticate()) {
+            BiometricManager.BIOMETRIC_SUCCESS ->
+                println("App can authenticate using biometrics.")
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE ->
+                println("No biometric features available on this device.")
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE ->
+                println("Biometric features are currently unavailable.")
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED ->
+                println("The user hasn't associated any biometric credentials with their account.")
+        }
     }
 }
